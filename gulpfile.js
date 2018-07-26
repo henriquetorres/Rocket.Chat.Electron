a6370e1168b46fae27d39c029383e7ce9a528cd8
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-07-26 17:20:46 -0300

    Move Gulp start task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const gulp = require('gulp');
+const electron = require('electron');
+const spawn = require('child_process').spawn;
+
 require('./tasks/build_app');
 require('./tasks/build_tests');
-require('./tasks/start');
+
+gulp.task('start', [ 'build', 'watch' ], () => {
+    spawn(electron, [ '.' ], { stdio: 'inherit' })
+        .on('close', () => process.exit());
+});
