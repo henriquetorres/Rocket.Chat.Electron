0ac944edacdc6bcbbd56271895a840963914d0e6
Tasso Evangelista <tasso.evangelista@rocket.chat>
2018-07-26 17:40:11 -0300

    Refactor build tasks

--- tasks/build_app.js
+++ tasks/build_app.js
@@ -11,46 +11,45 @@ const utils = require('./utils');
 
 const projectDir = jetpack;
 const srcDir = jetpack.cwd('./src');
+const configDir = jetpack.cwd('./config');
 const destDir = jetpack.cwd('./app');
 
-gulp.task('bundle', function () {
-    return Promise.all([
-        bundle(srcDir.path('background.js'), destDir.path('background.js')),
-        bundle(srcDir.path('app.js'), destDir.path('app.js')),
-        bundle(srcDir.path('i18n/index.js'), destDir.path('i18n/index.js')),
-        gulp.src(srcDir.path('public')+'/**/*').pipe(gulp.dest(destDir.path('public'))),
-        gulp.src(srcDir.path('i18n/lang')+'/**/*').pipe(gulp.dest(destDir.path('i18n/lang')))
-    ]);
-});
+gulp.task('bundle', () => Promise.all([
+    bundle(srcDir.path('background.js'), destDir.path('background.js')),
+    bundle(srcDir.path('app.js'), destDir.path('app.js')),
+    bundle(srcDir.path('i18n/index.js'), destDir.path('i18n/index.js'))
+]));
 
-gulp.task('less', function () {
-    return gulp.src(srcDir.path('stylesheets/main.less'))
+gulp.task('copy-assets', () => Promise.all([
+    gulp.src(srcDir.path('public')+'/**/*').pipe(gulp.dest(destDir.path('public'))),
+    gulp.src(srcDir.path('i18n/lang')+'/**/*').pipe(gulp.dest(destDir.path('i18n/lang')))
+]));
+
+gulp.task('less', () =>
+    gulp.src(srcDir.path('stylesheets/main.less'))
         .pipe(plumber())
         .pipe(less())
-        .pipe(gulp.dest(destDir.path('stylesheets')));
-});
+        .pipe(gulp.dest(destDir.path('stylesheets')))
+);
 
-gulp.task('environment', function () {
-    const configFile = 'config/env_' + utils.getEnvName() + '.json';
-    projectDir.copy(configFile, destDir.path('env.json'), { overwrite: true });
-});
+gulp.task('environment', () => projectDir.copy(configDir.path(`env_${ utils.getEnvName() }.json`),
+    destDir.path('env.json'), { overwrite: true }));
 
-gulp.task('watch', function () {
-    const beepOnError = function (done) {
-        return function (err) {
+gulp.task('watch', () => {
+    const beepOnError = done => {
+        return err => {
             if (err) {
                 utils.beepSound();
             }
+
             done(err);
         };
     };
 
-    watch('src/**/*.js', batch(function (events, done) {
-        gulp.start('bundle', beepOnError(done));
-    }));
-    watch('src/**/*.less', batch(function (events, done) {
-        gulp.start('less', beepOnError(done));
-    }));
+    watch(srcDir.path('.') + '/**/*.js', batch((events, done) => gulp.start('bundle', beepOnError(done))));
+    watch(srcDir.path('.') + '/{public,i18n}/**/*', batch((events, done) => gulp.start('copy-assets', beepOnError(done))));
+    watch(srcDir.path('.') + '/**/*.less', batch((events, done) => gulp.start('less', beepOnError(done))));
+    watch(configDir.path('.') + '/**/*', batch((events, done) => gulp.start('environment', beepOnError(done))));
 });
 
-gulp.task('build', ['bundle', 'less', 'environment']);
+gulp.task('build', [ 'bundle', 'copy-assets', 'less', 'environment' ]);
